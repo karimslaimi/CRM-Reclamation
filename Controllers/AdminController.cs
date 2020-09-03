710b9ae1f42f2baa9db37b1e2a406d2665e1f3b3
karim <karim-nar@live.fr>
2020-09-03 20:12:12 +0100

    some changes in the admin dashboard

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -43,6 +43,9 @@ namespace PFE_reclamation.Controllers {
             ViewBag.encourreclam = _reclams.Where(x => x.etat == Etat.En_cours).Count();
             ViewBag.nbreclam = _reclams.Count();
             ViewBag.clnb = db.Clients.Count();
+            ViewBag.clients = db.Clients.Include(s => s.Reclamations).Where(x => x.Reclamations.Count() > 0).OrderBy(x => x.Reclamations.Count()).Take(5).ToList();
+
+            ViewBag.lastreclams = db.Reclamations.Where(x => x.etat == Etat.Nouveau).OrderBy(s => s.debut_reclam).Take(5);
 
             return View();
             }
